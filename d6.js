3f7abf3078792c734a8f79a7af6cdabce99795fd
Duck-sot <raja07001@utb.vaxjo.se>
2025-01-22 11:38:38 +0100

    asap

--- d6.js
+++ d6.js
@@ -1,10 +1,17 @@
 function roll(){
     dice = document.getElementsByClassName("dice");
     let dmg = 0;
-    for(let i = 0; i <= 8; i++){
-        Number=Math.floor(Math.random()*6)+1;
-        dice[i].src = "dice" + Number + ".png" ;
-        dmg+=Number;
+    for(let i = 0; i < 8; i++){
+        number=Math.floor(Math.random()*6)+1;
+        dice[i].src = "dice" + number + ".png" ;
+        dmg+=number;
     }
-    document.getElementById
+     document.getElementById("dmg").innerHTML = dmg; 
+     document.getElementById("savedmg").innerHTML= dmg/2;
+}
+
+function sro(){
+    dice = getElementsByClassName("dice");
+    number= Math.floor(Math.random()*6)+1;
+    dice.src= "dice"+number+".png";
 }
