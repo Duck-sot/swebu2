ea08bcbb59fa583e2d6a4890936c7631cac2c42c
Duck-sot <raja07001@utb.vaxjo.se>
2025-01-24 13:41:52 +0100

    cart

--- d6.js
+++ d6.js
@@ -1,17 +1,18 @@
-function roll(){
+function roll() {
     dice = document.getElementsByClassName("dice");
     let dmg = 0;
-    for(let i = 0; i < 8; i++){
-        number=Math.floor(Math.random()*6)+1;
-        dice[i].src = "dice" + number + ".png" ;
-        dmg+=number;
+    for (let i = 0; i < 8; i++) {
+        number = Math.floor(Math.random() * 6) + 1;
+        dice[i].src = "dice" + number + ".png";
+        dmg += number;
     }
-     document.getElementById("dmg").innerHTML = dmg; 
-     document.getElementById("savedmg").innerHTML= dmg/2;
+    document.getElementById("dmg").innerHTML = dmg;
+    document.getElementById("savedmg").innerHTML = dmg / 2;
 }
 
-function sro(){
-    dice = getElementsByClassName("dice");
-    number= Math.floor(Math.random()*6)+1;
-    dice.src= "dice"+number+".png";
+function sro(id) {
+
+    number = Math.floor(Math.random() * 6) + 1;
+    document.getElementById(id).src = "dice" + number + ".png";
+
 }
